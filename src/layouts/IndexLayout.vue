2ec520e9dcd8a88d362fac2a10e3325a2012a377
limentic <nathan.lochon@etudiant.univ-lr.fr>
2022-03-29 15:18:46 +0200

    Initial commit

---
+++ src/layouts/IndexLayout.vue
@@ -0,0 +1,98 @@
+<template>
+  <q-layout view="lHh LpR lFf">
+    <q-drawer v-model="leftDrawerOpen" side="left" overlay behavior="mobile" bordered>
+      <q-list>
+        <q-item-label header>Menu</q-item-label>
+
+        <NavigationLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-page-sticky position="top-left" :offset="[10, 10]">
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        color="white"
+        aria-label="Menu"
+        @click="toggleLeftDrawer"
+      />
+    </q-page-sticky>
+  </q-layout>
+</template>
+
+<script>
+import NavigationLink from 'src/components/NavigationLink.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'IndexLayout',
+
+  components: {
+    NavigationLink
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,81 @@
+<template>
+  <q-page class="flex flex-center background-gradient">
+    <div class="flex flex-center column">
+      <h1 class="q-mt-xl q-mb-md text-bold text-white">JOUEZ AU UNO EN LIGNE</h1>
+      <div class="flex flex-center column" style="min-width: 30vw;">
+        <q-input v-model="username" dark borderless  input-class="text-center text-h4" color="white" placeholder="Saisissez votre pseudo" class="q-pb-md" style="width: 100%;" />
+        <q-btn color="white" label="Jouer !" text-color="black" to="/queue" />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  data () {
+    return {
+      helloWorldVar: 'helloWorld',
+      username: ''
+    }
+  },
+
+  mounted () {
+    this.helloWorld()
+  },
+
+  methods: {
+    helloWorld: function () {
+      console.log(this.helloWorldVar)
+    }
+  }
+})
+
+</script>
+
+<style scoped language="css">
+.background-gradient {
+  background: linear-gradient(45deg, #21d4fd, #b721ff);
+  background-size: 400% 400%;
+
+  -webkit-animation: MovingGradient 20s ease infinite;
+  -moz-animation: MovingGradient 20s ease infinite;
+  animation: MovingGradient 20s ease infinite;
+}
+
+@-webkit-keyframes MovingGradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+@-moz-keyframes MovingGradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+@keyframes MovingGradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
